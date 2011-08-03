5682243e9ebd267ba340663a05e63ea673a3ec90
Ignas Kaziukenas <the.qwer@gmail.com>
2011-08-03 14:43:31 +0300

    Initial commit

---
+++ src/api/festival/FestivalAndroidActivity.java
@@ -0,0 +1,13 @@
+package api.festival;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FestivalAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
