ca6541ab56d3d1e1ee136af622780d02a1ff07b4
Ignas Kaziukenas <the.qwer@gmail.com>
2011-08-03 19:40:09 +0300

    Loading is working

--- src/api/festival/FestivalAndroidActivity.java
+++ src/api/festival/FestivalAndroidActivity.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class FestivalAndroidActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -20,45 +22,68 @@ public class FestivalAndroidActivity extends Activity {
 	private Button ok;
 	private TextView entry;
 	private API api;
+	private ProgressDialog dialog;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
 		api = new API("3FPE9X151AMKIqrv", "V6KLOjmYaz8r_cYWTKIfVPfkHIiIj7Ha");
-		
+
 		list = (ListView) findViewById(R.id.list);
 		list.setAdapter(new ArrayAdapter<String>(this, R.layout.list, EVENTS));
-		
+
 		ok = (Button) findViewById(R.id.ok);
 		entry = (TextView) findViewById(R.id.entry);
 		ok.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View arg0) {
-				String entryText = entry.getText().toString();
-				
-				HashMap<String,String> map = new HashMap<String, String>();
-				map.put("title", entryText);
-				map.put("size", Integer.toString(size));
-				int from = 0;
-				map.put("from", Integer.toString(from));
-				Event[] events = api.getEvents(map);
-				
-				ArrayList<String> names = new ArrayList<String>();
-				
-				while (events.length != 0) {
-					for (Event event: events) {
-						names.add(event.getTitle());
-					}
-					from += size;
-					map.put("from", Integer.toString(from));
-					events = api.getEvents(map);
-				}
-				
-				ArrayAdapter<String> adapter = new ArrayAdapter<String>(list.getContext(), R.layout.list, names);
-				list.setAdapter(adapter);
+				dialog = ProgressDialog.show(FestivalAndroidActivity.this, "",
+						"Loading. Please wait...", true);
+				new EventThread(list).start();
 			}
-			
+
 		});
+	}
+
+	private class EventThread extends Thread {
+
+		private ListView listView;
+		private ArrayAdapter<String> adapter;
+
+		public EventThread(ListView listView) {
+			this.listView = listView;
+		}
+
+		public void run() {
+			String entryText = entry.getText().toString();
+
+			HashMap<String, String> map = new HashMap<String, String>();
+			map.put("festival", entryText);
+			map.put("size", Integer.toString(size));
+			int from = 0;
+			map.put("from", Integer.toString(from));
+			Event[] events = api.getEvents(map);
+
+			ArrayList<String> names = new ArrayList<String>();
+			while (events.length != 0) {
+				for (Event event : events) {
+					names.add(event.getTitle());
+				}
+				from += size;
+				map.put("from", Integer.toString(from));
+				events = api.getEvents(map);
+			}
+			adapter = new ArrayAdapter<String>(
+					list.getContext(), R.layout.list, names);
+			handler.sendEmptyMessage(0);
+		}
+
+		private Handler handler = new Handler() {
+			public void handleMessage(Message msg) {
+				listView.setAdapter(adapter);
+				dialog.dismiss();
+			}
+		};
 	}
 }
