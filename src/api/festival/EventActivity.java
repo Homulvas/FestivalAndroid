8bf974cb9bfd1c3374b199fee097bf22600003bf
Ignas Kaziukenas <the.qwer@gmail.com>
2011-08-05 16:40:46 +0300

    Performances list fully working.

--- src/api/festival/EventActivity.java
+++ src/api/festival/EventActivity.java
@@ -1,7 +1,10 @@
 package api.festival;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -11,18 +14,20 @@ public class EventActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.event);
 		
-		TextView festival = (TextView) findViewById(R.id.festival);
-		TextView description = (TextView) findViewById(R.id.description);
-		
 		Event event = (Event) getIntent().getSerializableExtra("event");
 		Performances[] performances = event.getPerformances();
 		
-		festival.setText(event.getTitle());
-		description.setText(event.getDescription());
-		
+		View headerView = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.event_footer, null, false); 
 		ListView performanceList = (ListView) findViewById(R.id.performanceList);
+		
 		PerformancesAdapter adapter = new PerformancesAdapter(this,
 				R.layout.performance_list, performances);
+		performanceList.addHeaderView(headerView);
 		performanceList.setAdapter(adapter);
+		
+		TextView festival = (TextView) findViewById(R.id.festival);
+		TextView description = (TextView) findViewById(R.id.description);
+		festival.setText(event.getTitle());
+		description.setText(event.getDescription());
 	}
 }
