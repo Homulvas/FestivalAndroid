8aded0684687496dfd30e2477a6432e0c80d3be2
Ignas Kaziukenas <the.qwer@gmail.com>
2011-08-29 18:48:52 +0300

    Building navigation support

--- src/api/festival/EventMap.java
+++ src/api/festival/EventMap.java
@@ -2,8 +2,13 @@ package api.festival;
 
 import java.util.List;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
@@ -27,6 +32,22 @@ public class EventMap extends MapActivity{
 	    List<Overlay> mapOverlays = mapView.getOverlays();
 	    Drawable drawable = this.getResources().getDrawable(R.drawable.maps);
 	    MapOverlay itemizedoverlay = new MapOverlay(drawable, this);
+	    
+	    LocationManager lMan = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+	    
+	    LocationListener locationListener = new LocationListener() {
+	        public void onLocationChanged(Location location) {
+	        	Toast.makeText(EventMap.this, "Trolololo", 100).show();
+	        }
+
+	        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+	        public void onProviderEnabled(String provider) {}
+
+	        public void onProviderDisabled(String provider) {}
+	      };
+	      
+	    lMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 0, locationListener);
 	    
 	    OverlayItem overlayitem = new OverlayItem(point, (String) getIntent().getSerializableExtra("title"), null);
 	    
