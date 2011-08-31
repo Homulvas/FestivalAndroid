0af7251c378de874f2044ce7b45e22e652e77b92
Ignas Kaziukenas <the.qwer@gmail.com>
2011-08-31 15:00:55 +0300

    User location is working without crashes. Updated images

--- src/api/festival/EventMap.java
+++ src/api/festival/EventMap.java
@@ -12,13 +12,13 @@ import android.os.Bundle;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
 
 public class EventMap extends MapActivity {
 	private static final int E06 = 1000000;
-	private LocationManager lMan;
-	private LocationListener locationListener;
+	private MyLocationOverlay locationOverlay;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,52 +26,21 @@ public class EventMap extends MapActivity {
 		MapView mapView = (MapView) findViewById(R.id.mapview);
 		mapView.setBuiltInZoomControls(true);
 
-		float longitude = (Float) getIntent().getSerializableExtra("longitude") * E06;
-		float latitude = (Float) getIntent().getSerializableExtra("latitude") * E06;
+		float longitude = (Float) getIntent().getSerializableExtra("longitude")
+				* E06;
+		float latitude = (Float) getIntent().getSerializableExtra("latitude")
+				* E06;
 
 		GeoPoint point = new GeoPoint((int) latitude, (int) longitude);
 
 		List<Overlay> mapOverlays = mapView.getOverlays();
-		Drawable drawable = this.getResources().getDrawable(R.drawable.maps);
+		Drawable drawable = this.getResources().getDrawable(R.drawable.pointer);
 		MapOverlay itemizedOverlay = new MapOverlay(drawable, this);
 
-		lMan = (LocationManager) this
-				.getSystemService(Context.LOCATION_SERVICE);
+		locationOverlay = new MyLocationOverlay(this,
+				mapView);
 
-		Drawable image = getResources().getDrawable(R.drawable.loc);
-		final MapOverlay locationOverlay = new MapOverlay(image, this);
-
-		locationListener = new LocationListener() {
-			OverlayItem pin = null;
-
-			public void onLocationChanged(Location location) {
-
-				if (pin == null) {
-					Location last = lMan.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-					GeoPoint loc = new GeoPoint((int) (last.getLatitude() * E06), (int) (last.getLongitude() * E06));
-					pin = new OverlayItem(loc, null, null);
-					locationOverlay.addOverlay(pin);
-				} else {
-					GeoPoint loc = new GeoPoint(
-							(int) (location.getLatitude() * E06),
-							(int) (location.getLongitude() * E06));
-					pin = new OverlayItem(loc, null, null);
-				}
-			}
-
-			public void onStatusChanged(String provider, int status,
-					Bundle extras) {
-			}
-
-			public void onProviderEnabled(String provider) {
-			}
-
-			public void onProviderDisabled(String provider) {
-			}
-		};
-
-		lMan.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100, 0,
-				locationListener);
+		locationOverlay.enableMyLocation();
 
 		OverlayItem overlayitem = new OverlayItem(point, (String) getIntent()
 				.getSerializableExtra("title"), null);
@@ -83,7 +52,12 @@ public class EventMap extends MapActivity {
 
 	public void onPause() {
 		super.onPause();
-		lMan.removeUpdates(locationListener);
+		locationOverlay.disableMyLocation();
+	}
+	
+	public void onResume() {
+		super.onResume();
+		locationOverlay.enableMyLocation();
 	}
 
 	protected boolean isRouteDisplayed() {
